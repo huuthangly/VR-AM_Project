73abe99a8b66857875a84b0a8e5fd31a35308bc2
AlienPC <84838571+huuthangly@users.noreply.github.com>
2025-01-24 18:14:16 -0500

    PrintTable_Object_Commit

---
+++ Content/Python/init_unreal.py
@@ -0,0 +1,18 @@
+import unreal
+import os
+from threading import Thread
+
+def executeTerminal(command):
+    os.system(command)
+    
+@unreal.uclass()
+class PrinterFunctions(unreal.BlueprintFunctionLibrary):
+
+    @unreal.ufunction(static = True, params = [str], ret = bool)
+    def PrintFile(FilePath):
+    
+        Thread(target=executeTerminal, args=(f"python3 C:\\Users\\Alien\\ankermake-m5-protocol\\ankerctl.py pppp print-file {FilePath}", )).start()
+        return True
+
+
+
